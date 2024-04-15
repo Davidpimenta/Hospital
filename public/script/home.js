a29152cce52c8627a00ebdd3252ef56f055d2676
David Sousa Pimenta <130236892+Davidpimenta@users.noreply.github.com>
2024-04-15 11:43:42 -0300

    VS 11

---
+++ public/script/home.js
@@ -0,0 +1,51 @@
+
+
+let col = document.createElement('div');
+col.className = (col);
+
+
+/* 
+
+const pedidos_div = document.getElementById('pedidos-div');
+
+let col = document.createElement('div');
+col.classList.add(col);
+
+let card = document.createElement('div');
+card.classList.add('card');
+
+let img = document.createElement('img');
+
+img.classList.add('card-img-top')
+img.src = '<?php echo $dado->img?>;';
+
+let card_body = createElement('div');
+card_body.classList('card_body');
+
+let h5 = document.createElement('h5');
+h5.classList.add('card-title');
+h5.innerText = '<?php echo $dado->pedido;?>';
+
+let p = document.createElement('p');
+p.classList.add('card-text');
+p.innerText = 'Entregue!';
+
+card_body.appendChild(h5);
+card_body.appendChild(p);
+card.appendChild(card_body);
+card.appendChild(img);
+col.appendChild(card);
+pedidos_div.appendChild(col);
+
+
+
+
+<div class="col">
+    <div class="card">
+      <img src="../images/ChocoCookieChantillyCup.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>*/
